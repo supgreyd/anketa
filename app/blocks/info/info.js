8fbe6fd940079d8b6684a440ad315ad1aa8c9cfa
supgreyd <supgreyd@gmail.com>
2017-08-24 19:21:08 +0300

    done

--- app/blocks/info/info.js
+++ app/blocks/info/info.js
@@ -1,50 +1,33 @@
-// function remakeValue(eFrom) {
-// 	const result = eFrom.value.split(' ')
-//
-// 	result[1] = result[1][0] + '.'
-// 	result[2] = result[2][0] + '.'
-//
-// 	return result.join(" ")
-// }
-//
-// (function(){
-// 	const fioContainer = document.getElementById('FIO');
-//
-// 	let fioDiv = fioContainer.getElementsByTagName('div')[0],
-// 		fioInput = fioContainer.getElementsByTagName('input')[0]
-//
-// 	let fioNaturalText = fioInput.value,
-// 		fioFakeText = ''
-//
-// 	fioDiv.addEventListener("click",function(e){
-// 		fioInput.focus()
-// 		fioInput.selectionStart = fioInput.value.length
-// 	}, false);
-//
-// 	fioInput.addEventListener("focus",function(e){
-// 		fioDiv.style.cssText = 'display: none'
-// 		fioInput.style.cssText = 'opacity: 1; position: static'
-// 	})
-//
-// 	fioInput.addEventListener("blur",function(e){
-// 		console.log('blur')
-// 		fioDiv.innerText = remakeValue(fioInput)
-// 		fioDiv.style.cssText = 'display: block'
-// 		fioInput.style.cssText = 'opacity: 0; position: absolute'
-// 	})
-//
-// })()
 
-let obj=document.getElementById("name");
-window.onresize = function () {
+let obj = document.getElementById("name");
+let el = obj.value;
+
+obj.addEventListener("blur", function(){
+	el = this.value
+})
+
+
+
+function fio() {
+
+	let arr = el.split(' ')
+	arr[1] = arr[1][0] + '.'
+	arr[2] = arr[2][0] + '.'
 	let width = window.innerWidth;
 	if( width < 500){
-		obj.value="Божик К. В.";
+		obj.value=arr.join(" ");
 	}
 	else if(width > 500){
-		obj.value="Божик Константин Викторович";
+		obj.value=el;
 	}
 }
 
+(function(){
+
+fio()
+
+})()
+
+window.onresize = fio
 
 
